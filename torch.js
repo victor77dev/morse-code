66f38ec5ddf1da4af242191742c2e26b9653807d
victor77dev <victor77.dev@gmail.com>
2020-08-02 14:06:29 +0200

    Testing

--- torch.js
+++ torch.js
@@ -1,4 +1,5 @@
 let torchOff = true;
+let track;
 
 const isOff = () => {
     console.log(torchOff)
@@ -9,48 +10,13 @@ const turnOff = () => {
     console.log('Turn Off')
     torchOff = !torchOff;
     //Test browser support
-    const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
-
-    if (SUPPORTS_MEDIA_DEVICES) {
-    //Get the environment camera (usually the second one)
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-    
-        const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-        if (cameras.length === 0) {
-        throw 'No camera found on this device.';
-        }
-        const camera = cameras[cameras.length - 1];
-
-        // Create stream and get video track
-        navigator.mediaDevices.getUserMedia({
-        video: {
-            deviceId: camera.deviceId,
-            facingMode: ['environment'],
-            height: {ideal: 1080},
-            width: {ideal: 1920}
-        }
-        }).then(stream => {
-        const track = stream.getVideoTracks()[0];
-
         //Create image capture object and get camera capabilities
-        const imageCapture = new ImageCapture(track)
-        const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
-
             //todo: check if camera has a torch
             track.applyConstraints({
                 advanced: [{torch: false}]
             });
-
-        });
-        });
-    });
     
-    //The light will be on as long the track exists
-    
-    }
 }
-
 
 const turnOn = () => {
     console.log('Turn On')
@@ -78,7 +44,7 @@ const turnOn = () => {
             width: {ideal: 1920}
         }
         }).then(stream => {
-        const track = stream.getVideoTracks()[0];
+        track = stream.getVideoTracks()[0];
 
         console.log('track', track)
         //Create image capture object and get camera capabilities
