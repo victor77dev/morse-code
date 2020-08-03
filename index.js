340321e4168b95cafc56bcf071a7718a0776681e
victor77dev <victor77.dev@gmail.com>
2020-08-04 00:14:18 +0200

    testing

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const log = document.querySelector('.log');
 
 const btn = document.querySelector('.switch');
 btn.addEventListener('click', function() {
-    window.console.log = (msg) => {
-        log.innerHTML += (typeof(msg) === 'object' ? JSON.stringify(msg) : msg) + '<br>';
-    }
+    // window.console.log = (msg) => {
+    //     log.innerHTML += (typeof(msg) === 'object' ? JSON.stringify(msg) : msg) + '<br>';
+    // }
     console.log('clicked');
     if (Torch.isOff()) {
         // Torch.turnOn();
         Torch.lightSeq([
-            [0, 1],
-            [3, 2],
-            [7, 1],
+            [0, 3],
+            [5, 2],
+            [9, 2],
         ])
     } else {
         Torch.turnOff();
